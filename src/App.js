e4806e485215cd93ca6e1f3152db0125252b7c49
Benjamin Barnard <barnardwben@yahoo.com>
2021-04-03 14:04:31 +0200

    Added Skeleton of Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import RandRM from "./RandRM";
+import SEO from "./SEO";
+import SMM from "./SMM";
+import Contact from "./Contact";
+import About from "./About";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ScrollToTop>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/seo">
+            <SEO />
+          </Route>
+          <Route exact path="/reviews-and-reputation-management">
+            <RandRM />
+          </Route>
+          <Route exact path="/social-media-marketing">
+            <SMM />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+        <Footer />
+      </ScrollToTop>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/RandRM.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RandRM = () => {
+  return <div></div>;
+};
+
+export default RandRM;
