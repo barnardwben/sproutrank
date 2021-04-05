0d5b917e5ed1572b17471de5859530640b0c1892
Benjamin Barnard <barnardwben@yahoo.com>
2021-04-05 15:08:57 +0200

    Added arrow click function

--- src/HomeHeader.js
+++ src/HomeHeader.js
@@ -4,6 +4,22 @@ const HomeHeader = () => {
   const circleOneRef = useRef(null);
   const circleTwoRef = useRef(null);
   const getStartedRef = useRef(null);
+  const homeHeadRef = useRef(null);
+  const arrowDownRef = useRef(null);
+
+  const arrowClick = () => {
+    window.scroll({
+      top: 640,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollHead = () => {
+    window.scrollY > 170
+      ? homeHeadRef.current.classList.add("head-scroll")
+      : homeHeadRef.current.classList.remove("head-scroll");
+  };
 
   const animateBubbles = () => {
     if (
@@ -21,19 +37,26 @@ const HomeHeader = () => {
       setTimeout(() => {
         getStartedRef.current.classList.add("getstarted-animation");
         getStartedRef.current.classList.remove("hide-getstarted-btn");
+        arrowDownRef.current.classList.add("arrow-down-animation");
+        arrowDownRef.current.classList.remove("hide-arrow-down");
       }, 2100);
     }
   };
 
   useEffect(() => {
     window.addEventListener("load", animateBubbles);
+    window.addEventListener("scroll", scrollHead);
+    document
+      .querySelector(".arrow-down-container")
+      .addEventListener("click", arrowClick);
     return () => {
       window.removeEventListener("load", animateBubbles);
+      window.removeEventListener("scroll", scrollHead);
     };
   }, []);
   return (
     <>
-      <header className="Home-Header">
+      <header ref={homeHeadRef} className="Home-Header">
         <section className="home-header-content">
           <div className="home-header-text">
             <div ref={circleOneRef} className="circle-one hide-circle-one">
@@ -53,6 +76,12 @@ const HomeHeader = () => {
             </button>
           </div>
         </section>
+        <div className="arrow-down-container ">
+          <i
+            ref={arrowDownRef}
+            className="fas fa-arrow-circle-down hide-arrow-down arrow-down"
+          ></i>
+        </div>
       </header>
     </>
   );
