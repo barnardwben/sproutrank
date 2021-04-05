77e6db3985a27aa93f474552a4777810b4f35cb4
Benjamin Barnard <barnardwben@yahoo.com>
2021-04-05 15:09:27 +0200

    Added HomeMain Component Content

---
+++ src/HomeMain.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+const HomeMain = () => {
+  return (
+    <>
+      <main className="Home-Main">
+        <section id="hmain-sec-one" className="hmain-section-one">
+          <h3>Lorem, ipsum dolor.</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
+            inventore, modi provident quam nostrum nihil vero hic perspiciatis
+            aliquid maxime dolore et nesciunt est?
+          </p>
+          <div className="hmain-secone-grid">
+            <div>
+              <h4>Lorem, ipsum.</h4>
+              <p>
+                <span>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident, ducimus quisquam. Similique facilis voluptatibus fuga
+                non!
+              </p>
+            </div>
+            <div>
+              <h4>Lorem, ipsum.</h4>
+              <p>
+                <span>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident, ducimus quisquam. Similique facilis voluptatibus fuga
+                non!
+              </p>
+            </div>
+            <div>
+              <h4>Lorem, ipsum.</h4>
+              <p>
+                <span>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident, ducimus quisquam. Similique facilis voluptatibus fuga
+                non!
+              </p>
+            </div>
+            <div>
+              <h4>Lorem, ipsum.</h4>
+              <p>
+                <span>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident, ducimus quisquam. Similique facilis voluptatibus fuga
+                non!
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default HomeMain;
